60ebd018f4e852b594334a11e5cfecb7f8baea5a
cindy 廖 <94544343+KittenSmasher@users.noreply.github.com>
2026-02-26 13:38:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var svgContainer = document.getElementById("svgMap");
 const isDev = location.origin.includes("app-dev.mekari.com");
 
 const API_BASE = isDev
-  ? "https://api-officeless-dev.mekari.com/28086/"
-  : "https://api-officeless.mekari.com/28086/";
+  ? "https://api-officeless-dev.mekari.com/28086"
+  : "https://api-officeless.mekari.com/28086";
 
 console.log("ENV:", isDev ? "dev" : "prod");
 console.log("API_BASE:", API_BASE);
